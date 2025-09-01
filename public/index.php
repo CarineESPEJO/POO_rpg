0262fe6d253113321f189fbe387fea90bfc7674e
CarineESPEJO <carine.espejo@gmail.com>
2025-09-01 09:52:23 +0200

    Premier commit : initialisation du projet

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<?php require_once __DIR__ . '../src/Character.php';
+$godzilla = new Character();
+$kong = new Character();
+    ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RPG</title>
+    <p><?php echo $godzilla ?> </p>
+    <p><?php echo $kong ?> </p>
+</head>
+<body>
+
+
+</body>
+</html>
